5bef376d0c719f9fd09134ac4b592b4b8491d649
William Will <10997562+willwill96@users.noreply.github.com>
2022-10-24 18:22:24 -0600

    rename to typescript

--- index.ts
+++ index.ts
@@ -1,1 +1,3 @@
-console.log("I'm a script from a package!")
+// Grab command argument. i.e
+const arg: string = process.argv[2]
+console.log("I'm a script from a package!\nThis is your argument: " + arg)
